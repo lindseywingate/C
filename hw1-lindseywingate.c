6e9ef7cf05a529e7e2235d06eccde9af599a86f1
Lindsey Wingate <lindseymwingate@MBP-de-Lindsey.T-mobile.com>
2016-01-23 21:05:29 -0600

    hw2 first draft

--- hw1-lindseywingate.c
+++ hw1-lindseywingate.c
@@ -1,34 +1,44 @@
 /*This code was written by Lindsey Wingate*/
 #include <stdio.h>
 
-int get_payment()
+float get_payment()
 {
-	int check;
-	printf("Please enter your monthly payment: \n");
-	scanf("%d", &check);
+	float check;
+	printf("Please enter your monthly payment more than $91: \n");
+	scanf("%f", &check);
 	return check;
 }
 
 int main() 
 {
-	int payment = get_payment();	
+	float payment = get_payment();	
 	float annual_interest = .22;
 	int total = 5000;
-	float first_month = annual_interest/12*total;
-/*	float other_months = rate/12*(total-payment+n)*/
-	printf("Monthly Payment: %d\n", payment);
-	printf("Interest Rate: %f\n", annual_interest); 	
-	printf("Total Cost: %d\n", total);
-	printf("First Month Interest Rate: %f\n", first_month);
+	float first_month = .22/12*total;
+	printf("------------\n"); 
+	printf("Monthly Payment: %.2f\n", payment);
+	printf("Interest Rate: %.2f\n", annual_interest); 	
+	printf("Total Cost: %.2d\n", total);
 
-	while (total>=0) 
+	printf("\n1\t%.2f\t%.2d\n", first_month, total);
+	float total_interest = first_month;	
+	float owe = total-payment+first_month;
+	int n = 1;
+
+	while (owe>0) 
 	{
-		total = total - 100;
-		printf("Just loopin it");
+		float interest = .22/12*(owe);
+		printf("%d\t%.2f\t%.2f\n",n,interest,owe);
+		owe = owe - payment + interest;
+		n = n + 1;
+		total_interest = total_interest + interest;
+
+		if (owe<=0)
+		{
+			printf("\nTotal Interest Paid: %.2f\n", total_interest); 
+		}
 	}	
+	
 
-
-return 0;
+	return 0;
 }
-
-
