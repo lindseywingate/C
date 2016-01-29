fc7b46c5ae407925404ed1dba9244fefa078e768
Lindsey Wingate <lindseymwingate@MBP-de-Lindsey.T-mobile.com>
2016-01-28 23:28:12 -0600

    printing file lines but maintains seg fault. unable to save file lines to array of structs ainda

--- hw2-lindseywingate.c
+++ hw2-lindseywingate.c
@@ -1,49 +1,30 @@
-/*This code was written by Lindsey Wingate*/
 #include <stdio.h>
 #include <stdlib.h> 
+#include <string.h>
 
-struct hw_data
-{
-	char name;
-	float f;
-	int i;
-	char desc;
-}
 
-/*Main allows the selection of 1-4 items. Does not exit menu until 5 is selected*/
 int main()
 {
 	int selection= 1;
-	struct hw_data data[];
-
 	while (selection!=5)
 	{
 		printf("1. Sort hw_data by the float value & print high to low\n2.Sort hw_data by the float value & print low to high\n3.Sort hw_data by the int value & print high to low\n4. Sort hw_data by the int value & print low to high\n5. Exit\n");
 		scanf("%d", &selection);
 	/*	printf("%d", selection);*/
 		
-		if (selection==1)
-		{
-			printf("test1");
-			openfile(data[]);
+		if (selection==1) {
+			open_file();
 		}
-		if (selection==2)
-		{
-			printf("test2");
-			openfile(data[]);	
+		if (selection==2) {
+			open_file();	
 		}
-		if (selection==3)
-		{
-			printf("test3");
-			openfile(data[]);
+		if (selection==3) {
+			open_file();
 		}
-		if (selection==4)
-		{
-			printf("test4");
-			openfile(data[]);
+		if (selection==4) {
+			open_file();
 		}	
-		if (selection==5)
-		{
+		if (selection==5) {
 			break;
 		}
 	}
@@ -51,22 +32,61 @@ int main()
 }
 
 /*opens file, stores hw_data in array. assumed no more than 100 entries*/
-int open_file(data[])
+int open_file()
 {
-	FILE*hw; /*file pointer allows program keep track of file being accessed*/
-	hw = *fopen("hw2.hw_data", "r");
-	
-	if (hw!=null)
-	{
-		char buffer[32];
-		while(!feof(hw))
+	struct data
 		{
-			fread(buffer, sizeof(char), sizeof(buffer), hw)
-		}
+			char name[42];
+			char f[10];
+			char i[10];
+			char desc[42];
+		};
+	
+	/*defining the structure to store file data*/
+	struct data hw_data[200]; /*array of structs*/
+	/*struct data *ptr = hw_data; declare pointer*/
+	char token[20], token2[20], token3[20], token4[20];
+	FILE*hw; /*hw pointer allows program keep track of file being accessed*/
+	char line[420];
+	int count = 0;
+	/*read each line into struct*/
+	
+	hw = fopen("hw2.data", "r");
+	while (!feof(hw)) { /*as i read the file, the pointer continues down each line*/
+		fgets(line, 420, hw);
+		strcpy(token, strtok(line, " "));
+		strcpy(token2, strtok(NULL, " "));
+		strcpy(token3, strtok(NULL, " "));
+		strcpy(token4, strtok(NULL, " "));
+		printf("%s\n", token);
+		printf("%s\n", token2);
+		printf("%s\n", token3);
+		printf("%s\n", token4);
+	/*	
+		hw_data[count].name = strcpy(*ptr,token);
+		hw_data[count].f = strcpy(*ptr, token2);
+		hw_data[count].i = strcpy(*ptr, token3);
+		hw_data[count].desc = strcpy(*ptr,token4);
+	*/	
+		/*
+		token=strtok(line,s);
+		print(token);
+		token2=strtok(NULL,s);
+		print(token2);
+		token3=strtok(NULL,s);
+		print(token3);
+		token4 = strtok(NULL,s);
+		print(token4);
+	
+		token=strtok(line,s);
+		
+		while (token!=NULL) {
+			printf("%s\n", token);
+			token = strtok(NULL, s);
+		}*/
+		count = count + 1;
+		/**ptr++;*/
 	}
-
-	int fclose(hw);
-	free(hw);/*frees up space allocated by malloc*/
-	return 0;
+	fclose(hw);
+	return 1;
 }
-/*This code was written by Lindsey Wingate*/
