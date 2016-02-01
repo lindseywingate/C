a7845e725b1de3625d1700a9d31fc1c1ea0bc039
Lindsey Wingate <lindseymwingate@MBP-de-Lindsey.T-mobile.com>
2016-01-31 18:46:17 -0600

    hw2 working copy added

--- hw2-lindseywingate.c
+++ hw2-lindseywingate.c
@@ -1,28 +1,39 @@
 #include <stdio.h>
-#include <stdlib.h> 
-#include <string.h>
+#include <stdlib.h>
+#include <string.h> 
 
+struct
+	{
+		char name[15];
+		float f;
+		int i;
+		char desc[15];
+	}a,hw_data[101];
 
 int main()
 {
 	int selection= 1;
 	while (selection!=5)
 	{
-		printf("1. Sort hw_data by the float value & print high to low\n2.Sort hw_data by the float value & print low to high\n3.Sort hw_data by the int value & print high to low\n4. Sort hw_data by the int value & print low to high\n5. Exit\n");
+		printf("1. Sort data by the float value & print high to low\n2.Sort data by the float value & print low to high\n3.Sort data by the int value & print high to low\n4. Sort data by the int value & print low to high\n5. Exit\n");
 		scanf("%d", &selection);
-	/*	printf("%d", selection);*/
-		
+		int sort_by, sort_by2, count;
+		count = setup();
 		if (selection==1) {
-			open_file();
+			sort_float(count);
+			printme2(count);
 		}
 		if (selection==2) {
-			open_file();	
+			sort_float(count);
+			printme(count);
 		}
 		if (selection==3) {
-			open_file();
+			sort_int(count);
+			printme2(count);
 		}
 		if (selection==4) {
-			open_file();
+			sort_int(count);
+			printme(count);
 		}	
 		if (selection==5) {
 			break;
@@ -32,61 +43,111 @@ int main()
 }
 
 /*opens file, stores hw_data in array. assumed no more than 100 entries*/
-int open_file()
+int setup( )
 {
-	struct data
-		{
-			char name[42];
-			char f[10];
-			char i[10];
-			char desc[42];
-		};
-	
-	/*defining the structure to store file data*/
-	struct data hw_data[200]; /*array of structs*/
-	/*struct data *ptr = hw_data; declare pointer*/
-	char token[20], token2[20], token3[20], token4[20];
 	FILE*hw; /*hw pointer allows program keep track of file being accessed*/
-	char line[420];
-	int count = 0;
 	/*read each line into struct*/
-	
+	int size = 101, n, count = 0, x;
+	char str[size];
+
 	hw = fopen("hw2.data", "r");
-	while (!feof(hw)) { /*as i read the file, the pointer continues down each line*/
-		fgets(line, 420, hw);
-		strcpy(token, strtok(line, " "));
-		strcpy(token2, strtok(NULL, " "));
-		strcpy(token3, strtok(NULL, " "));
-		strcpy(token4, strtok(NULL, " "));
-		printf("%s\n", token);
-		printf("%s\n", token2);
-		printf("%s\n", token3);
-		printf("%s\n", token4);
-	/*	
-		hw_data[count].name = strcpy(*ptr,token);
-		hw_data[count].f = strcpy(*ptr, token2);
-		hw_data[count].i = strcpy(*ptr, token3);
-		hw_data[count].desc = strcpy(*ptr,token4);
-	*/	
-		/*
-		token=strtok(line,s);
-		print(token);
-		token2=strtok(NULL,s);
-		print(token2);
-		token3=strtok(NULL,s);
-		print(token3);
-		token4 = strtok(NULL,s);
-		print(token4);
-	
-		token=strtok(line,s);
-		
-		while (token!=NULL) {
-			printf("%s\n", token);
-			token = strtok(NULL, s);
-		}*/
-		count = count + 1;
-		/**ptr++;*/
+	while(fscanf(hw, "%s %f %d %s", hw_data[count].name, &hw_data[count].f, &hw_data[count].i, hw_data[count].desc)>=1){
+		count++;	
 	}
 	fclose(hw);
-	return 1;
+	return count;
+}
+
+int printme2(int count)
+{
+	int test = count;
+	while(test>=0) {
+		if(hw_data[test].i!=0){
+			printf("%s %f %d %s\n", hw_data[test].name, hw_data[test].f, hw_data[test].i, hw_data[test].desc);
+		}
+		test--;
+	}
+	return 0;
+}
+int printme(int count)
+{
+	int test = 0;
+	while(test<count) {
+		if(hw_data[test].i!=0){
+			printf("%s %f %d %s\n", hw_data[test].name, hw_data[test].f, hw_data[test].i, hw_data[test].desc);
+		}
+		test++;
+	}
+	return 0;
+}
+
+int sort_int(int count)
+{
+	char s_temp[15];
+	float f_temp;
+	int i_temp;
+	int i=0, j=i+1, test=0;
+	
+	while(i<count){
+		j=i+1;
+		while(j<count){
+			if(hw_data[i].i>hw_data[j].i){
+				/*reassign name*/
+				strcpy(s_temp, hw_data[i].name);
+				strcpy(hw_data[i].name, hw_data[j].name);
+				strcpy(hw_data[j].name, s_temp);
+
+				/*reassign float*/
+				f_temp = hw_data[i].f;
+				hw_data[i].f = hw_data[j].f;
+				hw_data[j].f = f_temp;
+
+				/*reassign int*/
+				i_temp = hw_data[i].i;
+				hw_data[i].i = hw_data[j].i;
+				hw_data[j].i = i_temp;
+
+				/*reassign desc*/
+				strcpy(s_temp,hw_data[i].desc);
+				strcpy(hw_data[i].desc,hw_data[j].desc);
+				strcpy(hw_data[j].desc, s_temp);
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int sort_float(int count)
+{
+	char s_temp[15];
+	float f_temp;
+	int i_temp;
+	int i=0, j=i+1, test=0;
+	
+	while(i<count){
+		j=i+1;
+		while(j<count){
+			if(hw_data[i].f>hw_data[j].f){
+				/*reassign name*/
+				strcpy(s_temp, hw_data[i].name);
+				strcpy(hw_data[i].name, hw_data[j].name);
+				strcpy(hw_data[j].name, s_temp);
+				/*reassign float*/
+				f_temp = hw_data[i].f;
+				hw_data[i].f = hw_data[j].f;
+				hw_data[j].f = f_temp;
+				/*reassign int*/
+				i_temp = hw_data[i].i;
+				hw_data[i].i = hw_data[j].i;
+				hw_data[j].i = i_temp;
+				/*reassign desc*/
+				strcpy(s_temp,hw_data[i].desc);
+				strcpy(hw_data[i].desc,hw_data[j].desc);
+				strcpy(hw_data[j].desc, s_temp);
+			}
+			j++;
+		}
+		i++;
+	}
 }
