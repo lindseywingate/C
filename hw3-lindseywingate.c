b89e195a6c1295703f9e5067bdafeaaa7983d90a
Lindsey Wingate <lindseymwingate@gmail.com>
2016-02-09 23:19:41 -0600

    able to add file1 contents and print back out

--- hw3-lindseywingate.c
+++ hw3-lindseywingate.c
@@ -8,58 +8,78 @@ int main()
 	FILE*file1;
 	FILE*file2;
 	FILE*file3;
-//	int i;
+	
+	char *trash;
+	char *trash2;
+	char *trash3;
+	char line[50];
+
 	int count=0;
 	int count2=0;
 	int count3=0;
-	int counter=0;
-	int size;
-	char line[50];
-//	int size;
+	int size, y, i;
+	
+	char *data;
+	char **array;	
+
 	file1 = fopen("american0.txt", "r");
-	while(fscanf(file1, "%s", file1[count])>=1){
+	while(1) {
+		if (feof(file1)) {
+			break;
+		}
+		fscanf(file1, "%s", &trash);
 		count++;
 	}
 	printf("%d\n",count);
-	fclose(file1);
+	rewind(file1);
 	//return count;
 
 	file2 = fopen("american1.txt", "r");
-	while(fgets(line, "%s", file2)) {
+	while(1) {
+		if (feof(file2)) {
+			break;
+		}
+		fscanf(file2, "%s", &trash2);
 		count2++;
 	}
-	printf("%d\n",count2);	
+	printf("%d\n", count2);
+	rewind(file2);
 	
 	file3 = fopen("american2.txt", "r");
-	while(fgets(line, "%s", file3)) {
+	while(1) {
+		if (feof(file3)) {
+			break;
+		}
+		fscanf(file3, "%s", &trash3);
 		count3++;
 	}
-	printf("%d\n", count3);
+	printf("%d\n",count3);
+	rewind(file3);
 
 	size = count+count2+count3;
 	printf("%d\n", size);
 
-///creating one big array from files
-	char* array;
-	array = (char*)calloc(size, 50);
+	//column of pointers	
+	array = (char **)calloc(size, sizeof(array));
+	for (y=0; y<size; y++) {
+		//row of bytes
+		array[y] = (char *)calloc(50, 1);
+	}
 
-	printf("%d", counter);
-	file1 = fopen("american0.txt", "r");
-	while(fscanf(file1, "%c", &array[counter])>=1) {
-		counter++;
-	}
-	printf("%d", size);
-/*	printf("%d", counter);
-	fclose(file1);
-	file2 = fopen("american1.txt", "r");
-	while(fscanf(file2, "%c", &array[counter]>=1)) {
-		counter++;	
-	}
-*/
-	for(int i = 0; i<sizeof(array); i++) {
+	for(i=0; i<50; i++) {
+		fscanf(file1, "%c", &array[i]);
 		printf("%c", array[i]);
 	}
-
+/*    	
+	for(int j=count; j<count2; j++) {
+		fscanf(file2, "%s", &array[j]);
+		printf("%s\n", array[j]);
+	}	
+	free(array);
+*/
+	fclose(file1);
+	fclose(file2);
+	fclose(file3);	
 	return 0;
 }
 
