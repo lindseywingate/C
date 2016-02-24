bd016b7c8f7a3cff3e68865a461d588ad357ec12
Lindsey Wingate <lindseymwingate@gmail.com>
2016-02-23 21:52:46 -0600

    added a few things to the hw4

---
+++ hw4-lindseywingate.c
@@ -0,0 +1,30 @@
+/* This code was written by Lindsey Wingate*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+	char c;
+	printf("Please enter a character: ");
+	//if enter pressed, pass \0 to array, indicating string needs to be created and appended to HW 4 input: and then printed 
+	c = getchar();
+	putchar(c);
+	//vector_class(c);
+
+	return(0);	
+}
+
+/*char* vector_class()
+{
+//must use malloc/calloc to adjust array size- can be done in chunks of 5 or 10
+//read about memcpy
+//return pointer to array to main
+
+}
+
+*/
+
+/* This code was written by Lindsey Wingate*/
+
+
