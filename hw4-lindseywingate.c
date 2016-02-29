a7341d2adb3c52d484d5b3899f629b739a6766e0
Lindsey Wingate <lindseymwingate@gmail.com>
2016-02-29 00:43:34 -0600

    a little closer. just perfect passing enter to finish array and adjust array size. so... in reality a lot more work

--- hw4-lindseywingate.c
+++ hw4-lindseywingate.c
@@ -8,16 +8,15 @@ char* vector_class(char);
 int main()
 {
 	
-	unsigned char c=0;
+	char c="a";
 	do {
-	printf("Please enter a character. Press enter to quit: ");
+	printf("Please enter a letter. Press enter to quit: ");
 	//if enter pressed, pass \0 to array, indicating string needs to be created and appended to HW 4 input: and then printed 
 	c = getchar();
 	//putchar(c);
 	getchar();
 	printf("\n");
 	char* array_spot = vector_class(c);
-	printf("%p", array_spot);
 	vector_class(c);
 
 	}
@@ -31,8 +30,7 @@ char* vector_class(char var)
 	int count = 0;
 	char* array = (char *)malloc(5);		
 	array[count] = var;
-	for(int a = 0; a<5; a++)
-		printf("%c ARRAY COUNT \n", array[a]);
+	printf("%c var: \n", var);
 	count = count+1;
 //must use malloc/calloc to adjust array size- can be done in chunks of 5 or 10
 //read about memcpy
