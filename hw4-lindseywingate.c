65828f3300493c34c0855f3cf5cacf71697bdddf
Lindsey Wingate <lindseymwingate@gmail.com>
2016-02-29 16:33:22 -0600

    accepts chars into array and then creates new array when over 5

--- hw4-lindseywingate.c
+++ hw4-lindseywingate.c
@@ -3,42 +3,40 @@
 #include <stdlib.h>
 #include <string.h>
 
-char* vector_class(char);
-
+char* vector_class(char*, int);
+int SIZE = 0;
 int main()
 {
+	int count = 0;
+	char array[5];
+	char c;
+	char* p;
+	printf("Please enter a character. Press enter twice to quit: ");
+	while(1) {
+		if(c =='\n')
+			break;
+		//two getchar()s in a row get rid of extra characters (ex: \n)
+		c = getchar();
+		getchar();
+		p = &array[count];
+		array[count] = c;
+		count++;
+		//printf("size of array: %lu", sizeof(array));	
+		if(count==(sizeof(array)-1)) 
+			vector_class(p, count);
 	
-	char c="a";
-	do {
-	printf("Please enter a letter. Press enter to quit: ");
-	//if enter pressed, pass \0 to array, indicating string needs to be created and appended to HW 4 input: and then printed 
-	c = getchar();
-	//putchar(c);
-	getchar();
-	printf("\n");
-	char* array_spot = vector_class(c);
-	vector_class(c);
-
 	}
-	while(isalpha(c)!=0);
-	
-		return(0);	
+//	free(p);
+	return(0);	
 }
 
-char* vector_class(char var)
+char* vector_class(char* p, int count)
 {
-	int count = 0;
-	char* array = (char *)malloc(5);		
-	array[count] = var;
-	printf("%c var: \n", var);
-	count = count+1;
-//must use malloc/calloc to adjust array size- can be done in chunks of 5 or 10
-//read about memcpy
-//return pointer to array to main
-	free(array);
-	return(array);
+	printf("%d",count);
+	printf("does it even get here?");
+	char* new_array = (char*)calloc(count+1, sizeof(char));
+	printf("size of new_array: %lu", sizeof(new_array));
+	return(p2);
 }
 
 /* This code was written by Lindsey Wingate*/
-
-
