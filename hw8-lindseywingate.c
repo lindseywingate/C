7a0ea49987e01af71843594c31e1fe069b6db186
Lindsey Wingate <lindseymwingate@gmail.com>
2016-04-06 20:42:46 -0500

    started working on hw8

---
+++ hw8-lindseywingate.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct list
+{
+	char first_name[15];
+	char last_name[15];
+	struct Node* next;			
+}list; 
+
+int main(void)
+{
+	struct list s_array[10];//array of structs
+
+
+}
