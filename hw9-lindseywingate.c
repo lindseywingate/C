85e8fcdf4a7a1464bb7d2127f5b08172a979d695
Lindsey Wingate <lindseymwingate@gmail.com>
2016-04-18 21:23:58 -0500

    Finally got it to read from a file successfully

--- hw9-lindseywingate.c
+++ hw9-lindseywingate.c
@@ -1,14 +1,30 @@
 /*This code was written by Lindsey Wingate*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-int main(void)
+/*struct Node {
+	char data[50];
+	struct Node* next;
+	struct Node* prev;
+};
+struct Node* head;
+*/
+int main()
 {
-
-
-
+	int c;
+	FILE*file1;
+	char test;
+	
+	file1 = fopen("hw9.data", "r");
+	if(file1==NULL) {
+		printf("Can't open file.");
+		exit(1);
+	}	
+	if(file1) {
+		while((c=getc(file1)) !=EOF)
+			putchar(c);
+		fclose(file1);
+	}
 }
-
 /*This code was written by Lindsey Wingate*/
