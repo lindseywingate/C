878c5c19de73adb7c46ad0f887206958fce29272
Lindsey Wingate <lindseymwingate@gmail.com>
2016-04-20 00:05:41 -0500

    got it to read the nodes finally

--- hw9-lindseywingate.c
+++ hw9-lindseywingate.c
@@ -7,7 +7,10 @@ struct Node {
 	char data[50];
 	struct Node* next;
 	struct Node* prev;
+	struct Node* list;
+	int cost;
 };
+
 struct Node* nodester;
 
 //Inserts a Node at tail of Doubly linked list
@@ -26,28 +29,45 @@ void add(char x[50]) {
 	newNode->prev = temp;
 }
 
+void add_location(char x[50], char y[50], int num) {
+	struct Node* temp = nodester;
+	//finding matching location node
+	while(temp!=NULL) {
+		//if restaurant == restaurant searching for
+		if(strstr(temp->data, x)!=0)
+			break;
+		//if restaurant is not what we are looking for	
+		if(strstr(temp->data, x)==0){
+			if(temp->next==NULL)		
+				break;
+			temp=temp->next;
+		}
+	}
+	printf("PRINTING TEMP%s\n", temp->data);	
+	//find the head node (in doubly linked list)
+	struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+	//set data for new location node
+	strcpy(newNode->data, y);
+	newNode->list=NULL;
+	newNode->cost=num;
+	//find end of singly linked list
+	while(temp->list!=NULL)
+		temp=temp->list;
+	temp->list=newNode;
+}
+
 //Prints all the elements in linked list in forward traversal order
 void print() {
 	struct Node* temp = nodester;
+	struct Node* temp2;
 	while(temp != NULL) {
-		printf("%s\n",temp->data);
+		printf("MAIN NODE: %s\n",temp->data);
+		if(temp->list!=NULL) {
+			temp2=temp->list;
+			printf("Location Node: %s\n", temp2->data);
+			temp2=temp2->next;
+		}
 		temp = temp->next;
-	}
-	printf("\n");
-}
-
-//Prints all elements in linked list in reverse traversal order. 
-void print_reverse() {
-	struct Node* temp = nodester;
-	if(temp == NULL) return; // empty list, exit
-	// Going to last Node
-	while(temp->next != NULL) {
-		temp = temp->next;
-	}
-	// Traversing backward using prev pointer
-	while(temp != NULL) {
-		printf("Right to left output:	%s\n",temp->data);
-		temp = temp->prev;
 	}
 	printf("\n");
 }
@@ -58,15 +78,26 @@ int main()
 	FILE*file1;
 	char test[50];
 	char trash;
+	char token[50];
+	char token2[50];
+	int token3;
 	file1 = fopen("hw9data.txt", "r");
 	if(file1==NULL) {
 		printf("Can't open file.");
 		exit(1);
 	}
 	while(fgets(test, sizeof test, file1) !=NULL) {
-		if(strstr(test, "STOP") != 0)
-			break;
-		else {	
+		//if line has STOP, skip it
+		if(strstr(test, "STOP")!=0)
+			continue;
+		//if line has space, add travel node	
+		if(strstr(test, " ")!=0){
+			sscanf(test, "%s %s %d\n", token, token2, &token3);
+			printf("PRINTING: %s %s %d\n", token, token2, token3);
+			add_location(token, token2, token3);	
+		}
+		//if no spaces, add to doubly linked list
+		else {
 			add(test);	
 		}
 	}
